d565a0a7d790deae63bcbe493f5b9e87aa7fefe2
kawtar <kawtar.sabir200029@gmail.com>
2023-12-19 18:21:55 +0100

    create model and view of registre depart

---
+++ models/registre_depart.py
@@ -0,0 +1,5 @@
+from odoo import models, fields
+
+class BureauOrdreDocumentDepart(models.Model):
+    _name = 'bureau.ordre.document.depart'
+    _description = 'Gestion des Documents Depart au Bureau d\'Ordre'
